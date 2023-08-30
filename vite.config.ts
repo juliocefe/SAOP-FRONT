d9475471b078999792422da33937c8a9f5be98df
vicmms <34203591+vicmms@users.noreply.github.com>
2023-08-30 14:47:58 -0600

    fix repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://vicmms.github.io/saop/',
   plugins: [vue()],
   resolve: {
     alias: {
