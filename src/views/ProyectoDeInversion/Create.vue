4df41ba52276df8a0aaf958c45f8c0f5da38fba7
AliHCS <aligusali@gmail.com>
2023-10-24 18:02:37 -0600

    Se agrega la consulta de los catalogos

--- src/views/ProyectoDeInversion/Create.vue
+++ src/views/ProyectoDeInversion/Create.vue
@@ -14,19 +14,19 @@
           :error="errors"
           class="col-sm-6"
         />
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Prioridad:"
           placeholder="Prioridad"
           name="prioridad"
           id="prioridad"
           :error="errors"
           class="col-sm-6"
+          :options="arrayDataPrioridad.data"
+          :optionText="'descripcion'"
         />
       </div>
       <div class="row">
         <InputText
-          :disabled="itemId !== ''"
           v-model="data.clave"
           title="Clv Cartera:"
           placeholder="Clv Cartera"
@@ -35,37 +35,40 @@
           :error="errors"
           class="col-sm-6"
         />
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Factibilidad de la obra :"
           placeholder="Factibilidad de la obra"
           name="Factibilidad_de_la_obra"
           id="Factibilidad_de_la_obra"
           :error="errors"
           class="col-sm-6"
+          :options="arrayDataFactibilidad.data"
+          :optionText="'descripcion'"
         />
       </div>
       <div class="row">
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Area:"
           placeholder="Area"
           name="area"
           id="area"
           :error="errors"
           class="col-sm-12"
+          :options="arrayDataArea.data"
+          :optionText="'nombre'"
         />
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Tipo de Proyecto:"
           placeholder="Tipo de Proyecto"
           name="tipo_de_proyecto"
           id="tipo_de_proyecto"
           :error="errors"
           class="col-sm-12"
+          :options="arrayDataProyecto.data"
+          :optionText="'descripcion'"
         />
-        <InputText
-          v-model="data.descripcion"
+        Estatus del proyecto ????
+        <SelectComponent
           title="Estatus de Proyecto:"
           placeholder="Estatus de Proyecto"
           name="estatus_de_proyecto"
@@ -73,14 +76,15 @@
           :error="errors"
           class="col-sm-12"
         />
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Estatus:"
           placeholder="Estatus"
           name="estatus"
           id="estatus"
           :error="errors"
           class="col-sm-12"
+          :options="arrayDataEstatus.data"
+          :optionText="'descripcion'"
         />
         <InputText
           v-model="data.descripcion"
@@ -93,15 +97,6 @@
         />
       </div>
       <div class="row">
-        <InputText
-          v-model="data.descripcion"
-          title="Tipo:"
-          placeholder="Tipo de Proyecto"
-          name="tipo_de_proyecto"
-          id="tipo_de_proyecto"
-          :error="errors"
-          class="col-sm-12"
-        />
         <TextAraComponent
           :title="'Descripcion:'"
           :placeholder="'Ingresa una descripcion...'"
@@ -114,34 +109,37 @@
         {{ dataprueba }}
       </div>
       <div class="row">
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Pais:"
           placeholder="Pais"
           name="pais"
           id="pais"
           :error="errors"
           class="col-sm-6"
+          :options="arrayDataPais.data"
+          :optionText="'nombre_oficial'"
         />
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Entidad Federativa:"
           placeholder="Entidad Federativa"
           name="entidad_federativa"
           id="entidad_federativa"
           :error="errors"
           class="col-sm-6"
+          :options="arrayDataEntidadFederativa.data"
+          :optionText="'descripcion'"
         />
       </div>
       <div class="row">
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Unidad Responsable:"
           placeholder="Unidad Responsable"
           name="unidad_responsable"
           id="unidad_responsable"
           :error="errors"
           class="col-sm-12"
+          :options="arrayDataUnidadResponsable.data"
+          :optionText="'descripcion'"
         />
         <InputText
           v-model="data.descripcion"
@@ -207,14 +205,15 @@
           :error="errors"
           class="col-sm-6"
         />
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Fase:"
           placeholder="Fase"
           name="fase"
           id="fase"
           :error="errors"
           class="col-sm-6"
+          :options="arrayDataFase.data"
+          :optionText="'descripcion'"
         />
         <TextAraComponent
           :title="'Comentarios:'"
@@ -235,14 +234,15 @@
           :error="errors"
           class="col-sm-12"
         />
-        <InputText
-          v-model="data.descripcion"
+        <SelectComponent
           title="Tipo de Obra:"
           placeholder="Fase"
           name="fase"
           id="fase"
           :error="errors"
           class="col-sm-12"
+          :options="arrayDataTipoObra.data"
+          :optionText="'descripcion'"
         />
       </div>
       <div class="row">
@@ -251,14 +251,15 @@
         </div>
       </div>
       <div class="row">
-        <InputText
-          v-model="data.descripcion"
-          title="Tipo de Obra:"
+        <SelectComponent
+          title="Tipo Documento:"
           placeholder="Fase"
           name="fase"
           id="fase"
           :error="errors"
           class="col-sm-6"
+          :options="arrayDataTipoDocumento.data"
+          :optionText="'nombre'"
         />
         
         <div class="form-group col-sm-6">
@@ -293,6 +294,7 @@ import { scrollTop } from "@/utils/helpers/scrollHelper";
 import { useRoute, useRouter } from "vue-router";
 import { useForm } from "@/composables/useForm";
 import InputText from "@/components/InputText.vue";
+import SelectComponent from "@/components/SelectComponent.vue";
 import TextAraComponent from "@/components/TextAraComponent.vue";
 import { cat_faseValidations } from "@/utils/validations/cat_faseValidations";
 import usePetition from "@/composables/usePetition";
@@ -302,10 +304,23 @@ const route = useRoute();
 const router = useRouter();
 const itemId = ref("");
 const { updateData, getData, createData } = usePetition("cat_fase/");
+//Consultas para los selects
+const { arrayData:arrayDataPrioridad , getDatas: getDatasPrioridad } = usePetition("cat_prioridad/")
+const { arrayData:arrayDataFactibilidad , getDatas: getDatasFactibilidad   } = usePetition("cat_factibilidad/")
+const { arrayData:arrayDataArea , getDatas: getDatasArea   } = usePetition("cat_nivel_area/")
+const { arrayData:arrayDataProyecto , getDatas: getDatasProyecto   } = usePetition("cat_tipo_proyecto/")
+const { arrayData:arrayDataEstatus , getDatas: getDatasEstatus   } = usePetition("cat_estatus/")
+const { arrayData:arrayDataPais , getDatas: getDatasPais   } = usePetition("cat_pais/")
+const { arrayData:arrayDataEntidadFederativa , getDatas: getDatasEntidadFederativa  } = usePetition("cat_entidad_federativa/")
+const { arrayData:arrayDataUnidadResponsable , getDatas: getDatasUnidadResponsable  } = usePetition("cat_unidad_responsable/")
+const { arrayData:arrayDataFase , getDatas: getDatasFase  } = usePetition("cat_fase/")
+const { arrayData:arrayDataTipoObra , getDatas: getDatasTipoObra } = usePetition("cat_tipo_obra/")
+const { arrayData:arrayDataTipoDocumento , getDatas: getDatasTipoDocumento } = usePetition("cat_tipo_documento/")
 //Borrar estas variables
 const dataprueba = ref("");
 const datapruebaBeneficios = ref("");
 const datapruebaComentarios = ref("");
+
 
 const data = ref<CatFase>({
   clave: "",
@@ -351,6 +366,17 @@ onMounted(() => {
         router.push({ name: "listar-cat_Fase" });
       });
   }
+  getDatasPrioridad({ page: 1,size:100 })
+  getDatasFactibilidad({ page: 1,size:100 })
+  getDatasArea({ page: 1,size:100 })
+  getDatasProyecto({ page: 1,size:100 })
+  getDatasEstatus({ page: 1,size:100 })
+  getDatasPais({ page: 1,size:100 })
+  getDatasEntidadFederativa({ page: 1,size:100 })
+  getDatasUnidadResponsable({ page: 1,size:100 })
+  getDatasFase({ page: 1,size:100 })
+  getDatasTipoObra({ page: 1,size:100 })
+  getDatasTipoDocumento({ page: 1,size:100 })
   scrollTop();
 });
 </script>
