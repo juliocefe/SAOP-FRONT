e2790d393d3711aef519dc44f32b502a958eafa3
Gustavo Ali <aligusali@gmail.com>
2024-02-08 15:55:36 -0600

    Modal sin boton

---
+++ src/components/ModalWithoutButton.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <div
+      :class="{ 'show modal-backdrop': isModalOpen }"
+      v-if="isModalOpen"
+    ></div>
+    <div
+      class="modal fade"
+      :class="{ ' show ': isModalOpen }"
+      v-if="isModalOpen"
+    >
+      <div :class="['modal-dialog', sizeModal]" role="document">
+        <div class="modal-content">
+          <div class="custom-modal-header">
+            <h4 class="custom-modal-title">{{ props.title }}</h4>
+            <button
+              type="button"
+              class="close"
+              aria-label="Close"
+              @click="closeModal"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click="closeModal">
+              Cerrar
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary active"
+              @click="closeAndSaveModal"
+            >
+              {{ props.saveButtonTitle }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+  saveButtonTitle: {
+    type: String,
+    default: "Guardar",
+  },
+  largeModal: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["onSave", "onCloseModal"]);
+const isModalOpen = ref(true);
+
+const closeModal = () => {
+  isModalOpen.value = false;
+  emit("onCloseModal");
+};
+
+const closeAndSaveModal = () => {
+  isModalOpen.value = false;
+  emit("onCloseModal");
+  emit("onSave");
+};
+
+const sizeModal = props.largeModal ? "modal-lg" : "";
+</script>
+
+<style scoped>
+.custom-modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  border-bottom: 1px solid #545454;
+  background-color: #272829;
+}
+
+.custom-modal-title {
+  text-align: center;
+  color: white;
+  flex-grow: 1;
+}
+
+.custom-modal-header .close {
+  color: white;
+  cursor: pointer;
+}
+
+.custom-modal-header .close:hover {
+  color: #ccc;
+}
+</style>
